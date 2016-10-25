a94a407bc9a29717f32311793b2712a4225658c4
Administrator <fajer.slawek@gmail.com>
2016-10-25 17:15:41 +0200

    Add Algorithm file

---
+++ BuildingBlocks.BusinessLogic/Algorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildingBlocks.Models;
+
+namespace BuildingBlocks.BusinessLogic
+{
+    public class Algorithm
+    {
+        public List<Block> CalculatePositions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
