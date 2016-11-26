aa53a20c59b00d4646edea1b6a97bd65495e3f8a
Pawel <pawel.duszak@qualent.eu>
2016-11-26 13:20:37 +0100

    another blocklogicprobider test

--- BuildingBlocks.UnitTests/BlockLogicProviderTests.cs
+++ BuildingBlocks.UnitTests/BlockLogicProviderTests.cs
@@ -58,5 +58,42 @@ namespace BuildingBlocks.UnitTests
             Assert.AreEqual(result[3].Content[1, 0], true);
             Assert.AreEqual(result[3].Content[1, 1], true);
         }
+
+        /// <summary>
+        ///     Rotate block method test for sample correct block
+        /// </summary>
+        [Test]
+        public void RotateBlockTest_ForSampleCorrectBlock2()
+        {
+            var block = new Block()
+            {
+                Content = new[,]
+                {
+                    { true, true }
+                },
+                Width = 2,
+                Height = 1
+            };
+
+            var result = _blocksLogicProvider.RotateBlock(block);
+
+            Assert.AreEqual(result.Count, 4);
+            Assert.AreEqual(result[0].Width, 2);
+            Assert.AreEqual(result[0].Height, 1);
+            Assert.AreEqual(result[1].Width, 1);
+            Assert.AreEqual(result[1].Height, 2);
+            Assert.AreEqual(result[2].Width, 2);
+            Assert.AreEqual(result[2].Height, 1);
+            Assert.AreEqual(result[3].Width, 1);
+            Assert.AreEqual(result[3].Height, 2);
+            Assert.AreEqual(result[0].Content[0, 0], true);
+            Assert.AreEqual(result[0].Content[0, 1], true);
+            Assert.AreEqual(result[1].Content[0, 0], true);
+            Assert.AreEqual(result[1].Content[1, 0], true);
+            Assert.AreEqual(result[2].Content[0, 0], true);
+            Assert.AreEqual(result[2].Content[0, 1], true);
+            Assert.AreEqual(result[3].Content[0, 0], true);
+            Assert.AreEqual(result[3].Content[1, 0], true);
+        }
     }
 }
