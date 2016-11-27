f80ec00703188986cfce4727b2bf7b0b522a7438
Pawel <pawel.duszak@qualent.eu>
2016-11-27 16:32:15 +0100

    some algorithm test

--- BuildingBlocks.UnitTests/AlgorithmProviderTests.cs
+++ BuildingBlocks.UnitTests/AlgorithmProviderTests.cs
@@ -1,6 +1,8 @@
-﻿using BuildingBlocks.BusinessLogic.Algorithm;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BuildingBlocks.BusinessLogic.Algorithm;
 using BuildingBlocks.BusinessLogic.Interfaces;
-using Moq;
+using BuildingBlocks.Models.Models;
 using NUnit.Framework;
 
 namespace BuildingBlocks.UnitTests
@@ -23,9 +25,39 @@ namespace BuildingBlocks.UnitTests
         }
 
         [Test]
-        public void ExecuteTest_ForSampleCorrectData()
+        public async Task ExecuteTest_ForSampleCorrectData()
         {
-            
-        }   
+            const int step = 1;
+            const int k = 1;
+            var simulations = new List<Simulation>
+            {
+                new Simulation
+                {
+                    AvailableBlocks = new List<Block>
+                    {
+                        new Block
+                        {
+                            Content = new[,] { {true, true}},
+                            Height = 1,
+                            Id = 1,
+                            Width = 2,
+                            Quantity = 1
+                        }
+                    },
+                    Content = new[,] { {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} },
+                    WellHeight = 2,
+                    LastBlock = null
+                }
+            };
+
+            var result = await _algorithmSolver.Execute(simulations, k, step);
+
+            Assert.AreEqual(result.Count, 1);
+            Assert.AreEqual(result[0].Density, "1,00");
+            Assert.AreEqual(result[0].CanvasChildren.Count, 2);
+            Assert.AreEqual(result[0].AvailableBlocks.Count, 0);
+            Assert.AreEqual(result[0].Content[0, 14], 1);
+            Assert.AreEqual(result[0].Content[1, 14], 1);
+        }
     }
 }
